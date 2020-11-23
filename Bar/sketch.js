a2c332d8ffe25cb8fdc273afcf58edd3a39d785c
JosephAbbey <66320873+JosephAbbey@users.noreply.github.com>
2020-11-23 19:51:21 +0000

    Update sketch.js

--- Bar/sketch.js
+++ Bar/sketch.js
@@ -99,21 +99,21 @@ function draw() {
     box(size / 4, B4, size / 4);
     pop();
 
-    push();
-    fill(0);
-    translate(0, size / 2 + 1, 0);
-    angleMode(DEGREES);
-    rotateX(90);
-    plane(size);
-    pop();
     fill(255, 30);
     box(size);
-
     push();
     textAlign(CENTER, TOP);
     fill(255);
     translate(0, -size / 2, size / 2 + 1);
     textSize(size / 3);
     text(textCont, 0, 0);
+    pop();
+
+    push();
+    fill(0);
+    translate(0, size / 2 + 1, 0);
+    angleMode(DEGREES);
+    rotateX(90);
+    plane(size);
     pop();
 }
