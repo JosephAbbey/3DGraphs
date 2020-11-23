4fda06bc551cd9a9ea10e96e86dad9786c886641
JosephAbbey <66320873+JosephAbbey@users.noreply.github.com>
2020-11-23 15:13:16 +0000

    Update sketch.js

--- Line/sketch.js
+++ Line/sketch.js
@@ -6,16 +6,16 @@ function preload() {
 var size = 100;
 var theta = 0;
 var textCont = "Title";
-var X1 = 90,
+var X1 = 0,
     Y1 = 50,
     Z1 = 30;
-var X2 = 30,
+var X2 = 25,
     Y2 = 60,
     Z2 = 30;
-var X3 = 10,
+var X3 = 50,
     Y3 = 80,
     Z3 = 30;
-var X4 = 50,
+var X4 = 75,
     Y4 = 30,
     Z4 = 30;
 var spin = 0;
@@ -48,8 +48,9 @@ function draw() {
     push();
     fill(255, 0, 0);
     translate(size / 2, 0, 0);
-    line(0, 0, 0, X1, Y1, Z1);
     line(X1, Y1, Z1, X2, Y2, Z2);
+    line(X2, Y2, Z2, X3, Y3, Z3);
+    line(X3, Y3, Z3, X4, Y4, Z4);
     pop();
 
     push();
